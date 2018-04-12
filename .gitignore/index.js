ff8ac26af485e8b696d6566bcf11ad02d137f7fe
NeYziX <38168101+NeYziX@users.noreply.github.com>
2018-04-12 19:26:00 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -54,7 +54,7 @@ bot.on("guildMemberAdd", member => {
 });
 
 bot.on("guildMemberRemove", member => {
-    member.guild.channels.find("name", "bienvenue").send(`2sec...`)
+    member.guild.channels.find("name", "bienvenue").send(`${user.idname} a quitté PastelWorld. Nous te souhaitons une bonne continuation. Nous sommes **${server.member_count}**`)
 });
 
 bot.on("guildMemberAdd", member => {
