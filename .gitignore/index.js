3e8df25f1be626273aeda999f67f6aa72980b393
NeYziX <38168101+NeYziX@users.noreply.github.com>
2018-04-12 18:54:25 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const bot = new discord.Client();
+
+var prefix = ("&");
+
+bot.on("ready", function() {
+  bot.user.setGame("&help | By NeYziX")
+  console.log("le bot a démarré"); 
+});
+
+bot.login("NDM0MDMxODM2OTgxODg2OTg2.DbEfKg.MaBcnykEwD29xzVrNq7jQ6xX4oM")
