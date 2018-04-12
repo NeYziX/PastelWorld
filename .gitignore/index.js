8cc9a3221584d85f637d3a8a369fe16168b447f2
NeYziX <38168101+NeYziX@users.noreply.github.com>
2018-04-12 22:17:49 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -55,6 +55,13 @@ bot.on('message', message => {
 
 bot.on('message', message => {
 
+    if (message.content === prefix + "register") {
+        {!role {user} Minecraft}
+    }
+});
+
+bot.on('message', message => {
+
     if (message.content === prefix + "sm") {
         var embed = new Discord.RichEmbed()
         .setDescription("Réseaux sociaux de PastelWorld :")
