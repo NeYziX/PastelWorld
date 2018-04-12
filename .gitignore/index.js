d310aa2072e6891375cbe24694770b94b31429fa
NeYziX <38168101+NeYziX@users.noreply.github.com>
2018-04-12 20:06:32 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 var prefix = ("§");
 
 bot.on('ready', function() {
-  bot.user.setGame("§help | By NeYziX");
+  bot.user.setGame("§help | Dev By NeYziX");
   console.log("le bot a démarré"); 
 });
 
